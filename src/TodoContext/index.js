36aa4f0b61700f488129da7dc110f0edf356bb40
Eduardo Sandoval <eduardosandoval.atc@gmail.com>
2022-09-06 22:57:13 -0500

    LocalStorage

--- src/TodoContext/index.js
+++ src/TodoContext/index.js
@@ -1,11 +1,60 @@
 import React from "react";
+import { useLocalStorage } from "./use.LocalStorage";
 
 const TodoContext = React.createContext();
 
 function TodoProvider(props) {
-    retunr (
-<TodoContext.Provider value={{
+
+    const {
+        item: todos,
+        saveItem: saveTodos,
+        loading,
+        error,
+      } = useLocalStorage('TODOS_V1', []);
+      const [searchValue, setSearchValue] = React.useState('');
     
+      const completedTodos = todos.filter(todo => !!todo.completed).length;
+      const totalTodos = todos.length;
+    
+      let searchedTodos = [];
+    
+      if (!searchValue.length >= 1) {
+        searchedTodos = todos;
+      } else {
+        searchedTodos = todos.filter(todo => {
+          const todoText = todo.text.toLowerCase();
+          const searchText = searchValue.toLowerCase();
+          return todoText.includes(searchText);
+        });
+      }
+    
+      const completeTodo = (text) => {
+        const todoIndex = todos.findIndex(todo => todo.text === text);
+        const newTodos = [...todos];
+        newTodos[todoIndex].completed = true;
+        saveTodos(newTodos);
+      };
+    
+      const deleteTodo = (text) => {
+        const todoIndex = todos.findIndex(todo => todo.text === text);
+        const newTodos = [...todos];
+        newTodos.splice(todoIndex, 1);
+        saveTodos(newTodos);
+      };
+    
+
+    return (
+<TodoContext.Provider value={{
+          loading,
+          error,
+          totalTodos,
+          completedTodos,
+          searchValue,
+          setSearchValue,
+          searchedTodos,
+          completeTodo,
+          deleteTodo,
+
 }}>
     {props.children}
 </TodoContext.Provider>
@@ -13,4 +62,4 @@ function TodoProvider(props) {
     );
 }
 
-<TodoContext.Consumer></TodoContext.Consumer>
+export { TodoContext, TodoProvider };
