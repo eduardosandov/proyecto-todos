694ed106d59f1aa8a61ff6a43e8a29451c3a02fd
Eduardo Sandoval <eduardosandoval.atc@gmail.com>
2022-09-06 22:31:33 -0500

    TodoContext

--- src/App/index.js
+++ src/App/index.js
@@ -1,56 +1,14 @@
 import React from 'react';
 import { AppUI } from './AppUI';
 
-const defaultTodos = [
-  { text: 'Cortar cebolla', completed: true },
-  { text: 'Tomar el cursso de intro a React', completed: false },
-   { text: 'Llorar con la llorona', completed: true },
-   { text: 'LALALALAA', completed: false },
- ];
+//const defaultTodos = [
+//  { text: 'Cortar cebolla', completed: true },
+//  { text: 'Tomar el cursso de intro a React', completed: false },
+ //  { text: 'Llorar con la llorona', completed: true },
+//   { text: 'LALALALAA', completed: false },
+// ];
 
-function useLocalStorage(itemName, initialValue) {
-  const [error, setError] = React.useState(false);
-  const [loading, setLoading] = React.useState(true);
-  const [item, setItem] = React.useState(initialValue);
-  
-  React.useEffect(() => {
-    setTimeout(() => {
-      try {
-        const localStorageItem = localStorage.getItem(itemName);
-        let parsedItem;
-        
-        if (!localStorageItem) {
-          localStorage.setItem(itemName, JSON.stringify(initialValue));
-          parsedItem = initialValue;
-        } else {
-          parsedItem = JSON.parse(localStorageItem);
-        }
 
-        setItem(parsedItem);
-        setLoading(false);
-      } catch(error) {
-        setError(error);
-      }
-    }, 1000);
-  });
-  
-  const saveItem = (newItem) => {
-    try {
-      const stringifiedItem = JSON.stringify(newItem);
-      localStorage.setItem(itemName, stringifiedItem);
-      setItem(newItem);
-    } catch(error) {
-      setError(error);
-    }
-  };
-
-  return {
-    item,
-    saveItem,
-    loading,
-    error,
-  };
-}
 
 function App() {
   const {
--- src/TodoContext/index.js
+++ src/TodoContext/index.js
@@ -1,2 +1,16 @@
 import React from "react";
 
+const TodoContext = React.createContext();
+
+function TodoProvider(props) {
+    retunr (
+<TodoContext.Provider value={{
+    
+}}>
+    {props.children}
+</TodoContext.Provider>
+        
+    );
+}
+
+<TodoContext.Consumer></TodoContext.Consumer>
